38e011c795b0ccd3d967bc380c5a3cbe51fd6d82
suganuma <suganuma.itaru@gmail.com>
2017-05-02 18:16:11 +0900

    api

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,12 @@
 class PoemsController < ApplicationController
-def index
+
+  def index
     @poems = Poem.all
     render json: @poems
+  end
+
+  def show
+    @poem = Poem.find(params[:id])
+    render json: @poem
   end
 end
